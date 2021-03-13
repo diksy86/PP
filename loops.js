5dab79cc12d7f7f8784c66443b64787dd7207a72
Dijana <dijana.radojevic86@gmail.com>
2021-03-13 08:32:30 +0100

    Loops excercise

---
+++ loops.js
@@ -0,0 +1,154 @@
+/*
+1. Write a for loop that will iterate from 0 to 15. For each iteration, it will check if the current
+number is odd or even, and display a message to the screen.
+*/
+console.log("Zadatak 1");
+for (a = 0; a <= 15; a++) {
+    if (a % 2 === 0) {
+        console.log(a + " is even");
+    } else {
+        console.log(a + " is odd");
+    }
+}
+/*
+2. Write a program to sum the multiples of 3 and 5 under 1000.
+*/
+console.log("Zadatak 2");
+var sum = 0;
+for (var x = 0; x < 1000; x++)
+{
+    if (x % 3 === 0 || x % 5 === 0)
+    {
+       sum += x;
+    }
+}
+console.log(sum);
+/*
+3. Write a program to compute the sum and product of an array of integers.
+*/
+console.log("Zadatak 3");
+
+var numbers = [3,6,9];
+var sum = 0;
+var prod = 1;
+
+for (var i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+    prod *= numbers[i];
+
+}
+console.log("Sum is " + sum + " Product is " + prod);
+/*
+4. Write a program which prints the elements of the following array as a single string.
+var x = [&#39;1&#39;, &#39;A&#39;, &#39;B&#39;, &quot;c&quot;, &quot;r&quot;, true, NaN, undefined];
+*/
+console.log("Zadatak 4");
+
+var x = ['1', 'A', 'B', "c" , "r", true, NaN, undefined];
+
+var output = '';
+
+for (var i = 0; i < x.length; i++) {
+    output += x[i] + ", ";
+}
+console.log(output);
+//console.log(x.toString());
+/*
+5. Write a program that prints the elements of the following array.
+var a = [
+
+[1, 2, 1, 24],
+[8, 11, 9, 4],
+[7, 0, 7, 27]
+];
+*/
+console.log("Zadatak 5");
+
+var a = [ 
+    [1, 2, 1, 24], 
+    [8, 11, 9, 4], 
+    [7, 0, 7, 27] 
+];
+
+for (i = 0; i < a.length; i++) {
+    var subArray = a[i];
+
+    for (j = 0; j < subArray.length; j++) {
+        console.log( subArray[j] );
+    }
+}
+
+/*
+6. Write a program that outputs the sum of squares of the first 20 numbers.
+*/
+console.log("Zadatak 6");
+
+var sum = 0;
+
+for (var i = 1; i <= 20; i++) {
+    sum += i * i;
+}
+console.log(sum);
+/*
+7. Write a program that computes average marks of the following students. Then use this
+average to determine the corresponding grade.
+
+David 80
+Marko 77
+Dany 88
+John 95
+Thomas 68
+
+The grades are computed as follows :
+
+< 60% F
+< 70% D
+< 80% C
+
+< 90% B
+< 100% A
+*/
+console.log("Zadatak 7");
+
+var marks = [80,77,88,95,68]
+var sum = 0;
+for (var i = 0; i < marks.length; i++) {
+    sum += marks[i];
+}
+var avg = sum / marks.length;
+var grade = 'A';
+
+if (avg < 60) {
+    grade = "F";
+} else if (avg < 70) {
+    grade = "D";
+} else if (avg < 80) {
+    grade = "C";
+} else if (avg < 90) {
+    grade = "B";
+}
+console.log("Grade is " + grade);
+/*
+8. Write a program that uses console.log to print all the numbers from 1 to 100, with two
+exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers
+divisible by 5 (and not 3), print "Buzz" instead. When you have that working, modify your
+program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print
+"Fizz" or "Buzz" for numbers divisible by only one of those).
+Note: This is actually an interview question that has been claimed to weed out a significant
+percentage of programmer candidates. So if you’ve solved it, you’re now allowed to feel
+good about yourself.
+*/
+console.log("Zadatak 8");
+
+for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i + " FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log(i + " Fizz");
+    } else if (i % 5 === 0) {
+        console.log(i + " Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
---
+++ loops2.js
@@ -0,0 +1,201 @@
+/*
+1. Write a program that checks if a given element e is in the array a.
+Input: e = 3, a = [5, -4.2, 3, 7]
+Output: yes
+Input: e = 3, a = [5, -4.2, 18, 7]
+Output: no
+*/
+console.log("Zadatak 1");
+
+var e = 22;
+var a = [5, -4.2, 3, 7];
+var isFound = false;
+
+for (var i = 0; i < a.length; i++) {
+    if (a[i] === e) {
+        isFound = true;
+        break;
+    }
+}
+console.log(isFound ? "yes" : "no");
+
+/*
+2. Write a program that multiplies every positive element of a given array by 2.
+Input array: [-3, 11, 5, 3.4, -8]
+Output array: [-3, 22, 10, 6.8, -8]
+*/
+console.log("Zadatak 2");
+
+var a = [-3, 11, 5, 3.4, -8];
+
+for (var i = 0; i < a.length; i++) {
+    if (a[i] > 0) {
+        a[i] = a[i]*2;
+    }
+}
+console.log(a);
+
+
+/*
+3. Write a program that finds the minimum of a given array and prints out its value and
+index.
+Input array: [4, 2, 2, -1, 6]
+Output: -1, 3
+*/
+console.log("Zadatak 3");
+
+var a = [4, 2, 2, -1, 6];
+var min = false;
+var index = -1;
+
+for (var i = 0; i < a.length; i++) {
+    if (i === 0 || min > a[i]) {
+        min = a[i];
+        index = i;
+    }
+}
+
+console.log("Minimum value is " + min + " on index " + index);
+
+
+/*
+4. Write a program that finds the first element larger than minimum and prints out its value.
+Input array: [4, 2, 2, -1, 6]
+Output: 2
+*/
+console.log("Zadatak 4");
+
+var a = [4, 2, 2, -1, 6];
+var min = false;
+var min2 = false;
+
+for (var i = 0; i < a.length; i++) {
+    if (i === 0) {
+        min = a[i];
+    } else if ( a[i] < min ) {
+        min2 = min;
+        min = a[i];
+    }
+}
+
+/*
+5. Write a program that calculates the sum of positive elements in the array.
+Input array: [3, 11, -5, -3, 2]
+Output: 16
+*/
+console.log("Zadatak 5");
+
+var a = [3, 11, -5, -3, 2];
+var sum = 0;
+
+for (var i = 0; i < a.length; i++) {
+    if (a[i] > 0) {
+        sum += a[i]
+    }
+}
+console.log(sum);
+/*
+6. Write a program that checks if a given array is symmetric. An array is symmetric if it can
+be read the same way both from the left and the right hand side.
+Input array: [2, 4, -2, 7, -2, 4, 2]
+Output: The array is symmetric.
+Input array: [3, 4, 12, 8]
+Output: The array isn’t symmetric.
+*/
+console.log("Zadatak 6");
+
+var a = [3, 4, 5, 4, 3];
+var end = a.length - 1;
+var isSym = false;
+
+for (var start = 0; start < a.length; start++) {
+    if(start == end) { //neparan
+        break;
+    }
+    if(start > end) { //proslo kroz ceo niz
+        isSym = true;
+        break;
+    }
+
+   if(a[start] !== a[end]) { //ako nisu iste vrednosti
+        isSym = false;
+        break
+   }
+   end--; //smanjujemo index za po jedan
+}
+console.log(isSym ? "symetrical" : "not symetrical");
+
+
+
+/*
+7. Write a program that intertwines two arrays. You can assume the arrays are of the same
+length.
+Input arrays: [4, 5, 6, 2], [3, 8, 11, 9]
+Output array: [4, 3, 5, 8, 6, 11, 2, 9]
+*/
+console.log("Zadatak 7");
+
+var a = [4, 5, 6, 2];
+var b = [3, 8, 11, 9];
+var c = [];
+
+for (var i = 0; i < a.length; i++) {
+    c.push(a[i], b[i]);
+}
+console.log(c);
+/*
+8. Write a program that concatenates two arrays.
+Input arrays: [4, 5, 6, 2], [3, 8, 11, 9]
+Output array: [4, 5, 6, 2, 3, 8, 11, 9]
+*/
+console.log("Zadatak 8");
+
+var a = [4, 5, 6, 2];
+var b = [3, 8, 11, 9];
+var c = [];
+for (var i = 0; i < a.length; i++) {
+    c.push(a[i]); //ubacujemo a u c
+}
+for (var i = 0; i < b.length; i++) {
+    c.push(b[i]); //ubacujemo b u c
+}
+
+console.log(c);
+/*
+9. Write a program that deletes a given element e from the array a.
+Input: e = 2, a = [4, 6, 2, 8, 2, 2]
+
+Output array: [4, 6, 8]
+*/
+console.log("Zadatak 9");
+
+var a = [4, 6, 2, 8, 2, 2];
+var e = 2;
+var i = a.length - 1;
+
+while (i >= 0) {
+    if (a[i] === e) {
+        a.splice(i, 1);
+    }
+    i--;
+}
+
+console.log(a);
+
+
+
+/*
+10. Write a program that inserts a given element e on the given position p in the array a. If
+the value of the position is greater than the array length, print the error message.
+Input: e = 78, p = 3, a = [2, -2, 33, 12, 5, 8]
+Output: [2, -2, 33, 78, 12, 5, 8]
+*/
+console.log("Zadatak 10");
+var e = 78, p = 3, a = [2, -2, 33, 12, 5, 8];
+
+if (p > a.length) {
+    console.log("Error");
+} else {
+    a.splice(p, 0, e); //gde ubacujemo, koliko brisemo, sta stavljamo
+}
+console.log(a);
