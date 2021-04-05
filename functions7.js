8762cf86ecab61c27d63f5c9c901923f5984026b
Dijana <dijana.radojevic86@gmail.com>
2021-04-05 22:23:14 +0200

    Finished functions7.js

--- functions7.js
+++ functions7.js
@@ -1,11 +1,23 @@
-/*Why pay a fortune teller when you can just program your fortune yourself?
+/*
+1. Why pay a fortune teller when you can just program your fortune yourself?
 Write a function named tellFortune that:
 takes 4 arguments: number of children, partner's name, geographic location, job title.
 outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
 Call that function 3 times with 3 different values for the arguments.
 */
 
-/*You know how old your dog is in human years, but what about dog years? Calculate it!
+function tellFortune(num, pName, gLocation, job) {
+    var fortune = "You will be a " + job + " in " + gLocation + " and married to " + pName + " with " + num + " kids.";
+    return fortune;
+}
+
+console.log(tellFortune(2, "Nick", "Amsterdam", "teacher"));
+console.log(tellFortune(3, "Peter", "USA", "web designer"));
+console.log(tellFortune(5, "Sarah", "Spain", "dancer"));
+
+
+/*
+2.You know how old your dog is in human years, but what about dog years? Calculate it!
 
 Write a function named calculateDogAge that:
 takes 1 argument: your puppy's age.
@@ -16,7 +28,15 @@ Call the function three times with different sets of values.
 Bonus: Add an additional argument to the function that takes the conversion rate of human to dog years.
 */
 
-/*Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+function calculateDogAge(dogAge, humanAge) {
+    d_age = dogAge * 7;
+    h_age = humanAge / 7;
+    return "Dog age in human years is " + d_age + " and Human age in dog years is " + h_age;
+}
+console.log(calculateDogAge(5, 50));
+
+/*
+3.Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
 Write a function named calculateSupply that:
 takes 2 arguments: age, amount per day.
 calculates the amount consumed for rest of the life (based on a constant max age).
@@ -26,7 +46,16 @@ Call that function three times, passing in different values each time.
 Bonus: Accept floating point values for amount per day, and round the result to a round number.
 */
 
-/*It's hot out! Let's make a converter based on the steps here.
+function calculateSupply(age, amount) {
+    var maxAge = 90;
+    var supply = ((maxAge - age) * 365 * parseFloat(amount));
+    var output = "You will need " + supply + " candies to last you until the ripe old age of " + maxAge;
+    return output;
+}
+
+console.log(calculateSupply(30, 5.23));
+/*
+4.It's hot out! Let's make a converter based on the steps here.
 
 Create a function called celsiusToFahrenheit:
 Store a celsius temperature into a variable.
@@ -36,3 +65,14 @@ Create a function called fahrenheitToCelsius:
 Now store a fahrenheit temperature into a variable.
 Convert it to celsius and output "NN°F is NN°C."
 */
+function celsiusToFahrenheit(cel) {
+    var fah = cel * 9/5 + 32;
+    return cel + " °C is " + fah + " °F";
+}
+console.log(celsiusToFahrenheit(30));
+
+function fahrenheitToCelsius(fah) {
+    var cel = (5/9) * (fah - 32);
+    return fah + " °F is " + cel + " °C";
+}
+console.log(fahrenheitToCelsius(350));
