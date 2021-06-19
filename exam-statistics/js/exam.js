dfacafa9df0e29f2ff7995d2d3f5ebeae0a056b5
Dijana <dijana.radojevic86@gmail.com>
2021-06-19 13:01:25 +0200

    Exam Statistics Setup

---
+++ exam-statistics/js/exam.js
@@ -0,0 +1,3 @@
+//write a constructor function with three arguments that represent a subject, a student, and a grade.
+//add to its prototype a method getExamInfo that returns the name of the subject and the name and surname of the student.
+//add to its prototype a method hasPassed that checks if the student has passed the exam. As student has passed the exam if their grade is greater than 5.
---
+++ exam-statistics/js/subject.js
@@ -0,0 +1,2 @@
+// constructor function with two arguments that represent name and surname of the student
+//add to its prototype a method getStudentData that returns the name and surname of the student
